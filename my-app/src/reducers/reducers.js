0619be2dba35fb4f9645af0eee060f00c9407e47
Kennethaustin <kenneth.austin93@gmail.com>
2019-08-13 13:46:01 -0400

    initial state todo list

--- my-app/src/reducers/reducers.js
+++ my-app/src/reducers/reducers.js
@@ -1,4 +1,31 @@
 import {useReducer} from 'react';
+export const initialState = {
+    todo: [
+        {
+            item: 'Learn Dark mode themes',
+            completed: false,
+            id: 1234
+        },
+
+        {
+            item: 'Listen to Syntax Podcast',
+            completed: false,
+            id: 1235
+        },
+
+        {
+            item: 'Watch one creative coding video',
+            completed: false,
+            id: 1236
+        },
+
+        {
+            item: 'Create A small react app',
+            completed: false,
+            id: 1237
+        }
+    ]
+};
 
 export default reducer= (state, action) => {
     switch(action.type){
